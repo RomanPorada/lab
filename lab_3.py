53bb407d6508b69b68bf86612ccac778f85dfdd5
RomanPorada <romanporada32@gmail.com>
2024-10-07 19:44:00 +0300

    lab 3

--- lab_3.py
+++ lab_3.py
@@ -3,29 +3,33 @@ import random
 def generate_random_list(n, min_value, max_value):
     return [random.randint(min_value, max_value) for _ in range(n)]
 
+def sum(list):
+    sum = 0
+    for num in list:
+        sum += num
+    return sum
+
+
 def min_value_to_balance(list):
     n = len(list)
     if n % 2 != 0:
         raise ValueError("Послідовність повинна мати парну кількість елементів")
     
+
     left_half = list[:n//2]
     right_half = list[n//2:]
     
-    sum_left = 0
-    for num in left_half:
-        sum_left += num
+    sum_left = sum(left_half)
     
-    sum_right = 0
-    for num in right_half:
-        sum_right += num
+    sum_right = sum(right_half)
     
     difference = abs(sum_left - sum_right)
     return difference * -1
     
 
-n = 6
-min_value = 1
-max_value = 18
+n = int(input("Введіть значення довжини списку(воно має бути парним): "))
+min_value = int(input("Введіть початок відрізка з якого буде формуватися список: "))
+max_value = int(input("Введіть кінець відрізка з якого буде формуватися список: "))
 
 # Генерація випадкового списку
 random_list = generate_random_list(n, min_value, max_value)
@@ -33,3 +37,11 @@ random_list = generate_random_list(n, min_value, max_value)
 # Виведення результатів
 print("Випадкова послідовність:", random_list)
 print("Мінімальне значення для балансування:", min_value_to_balance(random_list))
+
+dic = {
+    "yes": True,
+    "no": False
+}
+
+del(dic["no"])
+print(dic)
