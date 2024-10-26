44d09e3ae8183dcdd7198e5d2120bee4453a4468
RomanPorada <romanporada32@gmail.com>
2024-10-26 11:54:02 +0300

    lab 5

--- atom.py
+++ atom.py
@@ -13,18 +13,20 @@ class Atom:
     neutrons_number: int
     protons_number : int 
     electrons_number : int
+    chemical_record : str
     atom_tupe : AtomType
 
-    def __init__(self, name = "невідомо", atomic_mass_unit = 1, neutrons_number = 1, protons_number = 1, electrons_number = 1, atom_tupe = AtomType.STABLE):
+    def __init__(self, name = "невідомо", atomic_mass_unit = 1, neutrons_number = 1, protons_number = 1, electrons_number = 1, chemical_record = "Невідомо", atom_tupe = AtomType.STABLE):
         self.name = name
         self.atomic_mass_unit = atomic_mass_unit
         self.neytrons_number = neutrons_number
         self.protons_number = protons_number
         self.electrons_number = electrons_number
+        self.chemical_record = chemical_record
         self.atom_tupe = atom_tupe
 
     def is_neutral(self) -> bool:
         return self.neytrons_number == self.electrons_number
         
     def __repr__(self):
-        return f"{self.name}(mass: {self.atomic_mass_unit}, neutrons: {self.neutrons_number}, protons: {self.protons_number}, electrons: {self.electrons_number}, type: {self.atom_type.name})"
+        return f"{self.name}(mass: {self.atomic_mass_unit}, neutrons: {self.neutrons_number}, protons: {self.protons_number}, electrons: {self.electrons_number}, type: {self.atom_tupe.name})"
--- lab_5.py
+++ lab_5.py
@@ -4,7 +4,7 @@ import periodic_system
 class Molecule:
     __name : str
     __atoms : list
-    def __init__(self, name = "Навідомо", atoms = []):
+    def __init__(self, name = "Невідомо", atoms = []):
         self.__name = name
         self.__atoms = atoms
     
@@ -16,6 +16,42 @@ class Molecule:
         for el in self.__atoms:
             atoms.append(el.name)
         return f"Molecule: (name = {self.__name}, atoms = {atoms})"
+    
+    def formula(self, sort = True):
+        if sort == True:
+            self.sort_atoms()
+        else: 
+            pass
+        previous_atom = ""
+        quantity = 0
+        current_formul = ""
+        final_formul = ""
+        for atom in self.__atoms:
+            if atom.chemical_record == previous_atom:
+                previous_atom = atom.chemical_record
+                quantity += 1
+                current_formul = atom.chemical_record + f"{quantity}" 
+            else:
+                if quantity == 0:
+                    previous_atom = atom.chemical_record
+                    quantity += 1
+                else:
+                    final_formul += current_formul
+                    current_formul = ""
+                    previous_atom = atom.chemical_record
+                    quantity = 1
+                    current_formul += atom.chemical_record + (f"{quantity}" if quantity > 1 else "")
+        final_formul += current_formul
+        return final_formul
+
+    def find_mass(self):
+        if not atoms: 
+            return 0
+        total_mass = 0
+        for atom in self.__atoms:
+            total_mass += atom.atomic_mass_unit
+        return total_mass
+    
 
 def find_average_mass(atoms: list[Atom]):
     if not atoms: 
@@ -31,8 +67,11 @@ atoms = [getattr(periodic_system, name.strip()) for name in atoms_names]
 molecule = Molecule(name, atoms)
 
 print(molecule)
+print(molecule.formula(sort=False))
 molecule.sort_atoms()
 print(molecule)
+print(molecule.find_mass())
+
 
 print(periodic_system.B_ISO.is_neutral())
 print(periodic_system.AR_STA.is_neutral())
